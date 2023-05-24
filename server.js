7321a4780bf4d1efea47b104645bd5f258116928
Mahimna P <mahimnap@gmail.com>
2023-05-24 06:22:43 -0400

    basic backend with no framework. Only get request from frontend to allow for easier backend route testing

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express'); //npm installed
-const app = express(); 
-const path = require('path'); 
+const http = require ('http'); 
+const querystring = require('querystring'); 
 
-app.get ('/', (req, res) => { //these can be named anything but its usually called this 
-    const filePath = path.join(__dirname, 'public', 'index.html'); //absolute path 
-    res.sendFile(filePath); //send landing page
-});
+const server = http.createServer((req, res) => {
+    if (req.url === '/'){
+        console.log ('Greet successful. Now implement response'); 
+        
+    }
+    else {
+        res.writeHead(404); 
+        res.end('Not Found'); 
+    }
+}); 
 
-app.post('/signIn', (req, res) => {
-    console.log ('post works'); 
-    res.redirect('/'); 
-})
-
-app.listen(3000, () => {
-    console.log('Server Listening on http://localhost:3000'); 
-});
+    server.listen(3000, () => {
+        console.log('Server listening on http://localhost:3000/');
+    })
